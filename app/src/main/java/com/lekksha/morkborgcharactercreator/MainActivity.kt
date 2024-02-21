6fbd433bb097c5caae4847c86db32912cb0f3754
lekksha_ <lekksha@gmail.com>
2024-02-21 22:03:31 +0700

    functionality to randomize names

--- app/src/main/java/com/lekksha/morkborgcharactercreator/MainActivity.kt
+++ app/src/main/java/com/lekksha/morkborgcharactercreator/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.lekksha.morkborgcharactercreator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.lekksha.morkborgcharactercreator.databinding.ActivityMainBinding
 
@@ -13,6 +14,19 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.textViewName.typeface = ResourcesCompat.getFont(this, R.font.rakkas)
+        changeTextViewFontToRakkas(binding.textViewName)
+
+        // Randomizing stuff below
+
+        // Randomizes name of the character
+        // TODO: Refactor to one method
+        val names = resources.getStringArray(R.array.random_names)
+        val randomString = names.random()
+        binding.textViewName.text = randomString
+
+    }
+
+    private fun changeTextViewFontToRakkas(textView: TextView) {
+        textView.typeface = ResourcesCompat.getFont(this, R.font.rakkas)
     }
 }
