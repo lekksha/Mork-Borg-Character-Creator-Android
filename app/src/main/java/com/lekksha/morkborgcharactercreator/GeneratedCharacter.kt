a2c9f8f1f7a8886122b4376717db0d2ffbdeb957
lekksha_ <lekksha@gmail.com>
2024-04-30 20:16:35 +0700

    create class for holding data about generated character

---
+++ app/src/main/java/com/lekksha/morkborgcharactercreator/GeneratedCharacter.kt
@@ -0,0 +1,14 @@
+package com.lekksha.morkborgcharactercreator
+
+class GeneratedCharacter {
+    val description : MutableList<String> = mutableListOf<String>()
+    val characterClass : String = "0"
+    val abilities : MutableList<String> = mutableListOf<String>()
+    val stats : MutableList<String> = mutableListOf<String>()
+    val armor : String = "0"
+    val weapons : MutableList<String> = mutableListOf<String>()
+    val silver : Int = 0
+    val hp : Int = 0
+    val omens : Int = 0
+    val powers : MutableList<String> = mutableListOf<String>()
+}
