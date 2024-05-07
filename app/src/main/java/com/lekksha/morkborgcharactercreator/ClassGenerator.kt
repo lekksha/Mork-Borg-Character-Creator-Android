07994d901d7866e273fcaa523d9251c5b861a983
lekksha_ <lekksha@gmail.com>
2024-05-07 16:45:13 +0700

    add secondary item generation

--- app/src/main/java/com/lekksha/morkborgcharactercreator/ClassGenerator.kt
+++ app/src/main/java/com/lekksha/morkborgcharactercreator/ClassGenerator.kt
@@ -7,9 +7,9 @@ abstract class ClassGenerator {
         val gen = GeneratedCharacter()
         gen.characterClass = chooseClass(context)
         gen.abilities = getAbilities()
-        gen.equipment = generateEquipment(context)
-        gen.description = generateDescription()
         gen.stats = generateStats()
+        gen.equipment = generateEquipment(context, gen.stats[2])
+        gen.description = generateDescription()
         gen.armor = generateArmor()
         gen.weapons = generateWeapon()
         gen.silver = generateSilver()
@@ -27,14 +27,15 @@ abstract class ClassGenerator {
         return mutableListOf("None")
     }
 
-    protected fun generateEquipment(context : Context) : MutableList<String> {
+    protected fun generateEquipment(context : Context, statPresence: Int) : MutableList<String> {
         val equipment = mutableListOf<String>()
         equipment.addAll(generateFood(context))  // TODO: create an option to generate food and drinks separately
         equipment.addAll(generateContainer(context))
-        equipment.add(generateSecondItem())
+        equipment.add(generateSecondItem(context, statPresence))
         equipment.add(generateThirdItem())
         return equipment
     }
+
 
     private fun generateFood(context: Context): MutableList<String> {
         val res = mutableListOf<String>()
@@ -51,6 +52,44 @@ abstract class ClassGenerator {
             return mutableListOf<String>() // Also can returns only in case
         else return mutableListOf(context.resources.getStringArray(R.array.containers)[containerIndex])
     }
+
+
+    private fun generateSecondItem(context: Context, statPresence : Int): String {
+        val containerIndex = Random.nextInt(0, context.resources.getStringArray(R.array.second_items).size)     // TODO: create an option to select "above" variants of containers
+        when (containerIndex) {
+            1 -> {
+                return (statPresence + 4).toString() + " " + context.resources.getStringArray(R.array.second_items)[containerIndex]
+            }
+            2 -> {
+                return context.resources.getStringArray(R.array.second_items)[containerIndex] + " " + (statPresence + 6).toString()
+            }
+            4 -> {
+                return context.resources.getString(R.string.unclean_scroll) + ": " +
+                        context.resources.getStringArray(R.array.unclean_scrolls)[Random.nextInt(
+                            0,
+                            context.resources.getStringArray(R.array.unclean_scrolls).size
+                        )]
+            }
+            6 -> {
+                return context.resources.getStringArray(R.array.second_items)[containerIndex] + " " + (statPresence + 4).toString()
+            }
+            10 -> {
+                return context.resources.getStringArray(R.array.second_items)[containerIndex] + " " + Random.nextInt(1, 4+1).toString()
+            }
+            else -> {
+                return context.resources.getStringArray(R.array.second_items)[containerIndex]
+            }
+        //        if (containerIndex !in 0..1)
+//            return mutableListOf<String>() // Also can returns only in case
+//        else return mutableListOf(context.resources.getStringArray(R.array.containers)[containerIndex])
+        }
+    }
+
+
+    private fun generateThirdItem(): String {
+
+    }
+
 
     protected abstract fun generateDescription() : MutableList<String>
    /* TODO: Generation of stats requires users selection of two out of three ability scores */
