96cd916481fc86803039528c509aab1df382391f
lekksha_ <lekksha@gmail.com>
2024-05-12 16:53:57 +0700

    add ability to delete textfields

--- app/src/main/java/com/lekksha/morkborgcharactercreator/MainActivity.kt
+++ app/src/main/java/com/lekksha/morkborgcharactercreator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lekksha.morkborgcharactercreator
 
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -25,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         binding.textViewName.text = generateFromResource(R.array.random_names)
 
         putUpGeneratedCharacter(ClassGenerator().run(this))
-        addAddDescriptionButton()
+        addAddDescriptionButton()   // TODO: Refactor insides of the function
+        addAddPowerButton()
 
         binding.textViewName.setOnClickListener {
             binding.textViewName.text = generateFromResource(R.array.random_names)
@@ -33,16 +35,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun addAddDescriptionButton() {
-        val button = createAddDescriptionButton()
-        binding.linearLayoutDiscription.addView(button)
-        button.setOnClickListener {
-            val textInputLayout = createDescriptionField()
+    private fun addAddPowerButton() {
+        val buttonAddPower = createAddPowerButton()
+        addAddTextFieldFunctionalityToButton(buttonAddPower, binding.LinearLayoutPowers)
+    }
 
-            binding.linearLayoutDiscription.removeView(button)
-            binding.linearLayoutDiscription.addView(textInputLayout)
-            binding.linearLayoutDiscription.addView(button)
+    private fun createAddPowerButton(): MaterialButton {
+        val button = MaterialButton(this, null, com.google.android.material.R.attr.materialIconButtonStyle)
+        button.text = "Add Power"
+        button.setIconResource(R.drawable.ic_add_24)
+        return button
+    }
+
+
+    private fun addAddDescriptionButton() {
+
+        val descriptionAddButton = createAddDescriptionButton()
+        addAddTextFieldFunctionalityToButton(descriptionAddButton, binding.linearLayoutDiscription)
+    }
+
+    private fun addAddTextFieldFunctionalityToButton(button: MaterialButton, placeToAdd : LinearLayout) {
+        placeToAdd.addView(button)
+        button.setOnClickListener {
+            val textFieldAndCross = layoutInflater.inflate(R.layout.additional_textfield, null)
+            textFieldAndCross.findViewById<View>(/* id = */ R.id.imageButton).apply {
+                setOnClickListener {
+                    placeToAdd.removeView(textFieldAndCross)
+                }
+            }
+            addTextFieldBelow(button, textFieldAndCross, placeToAdd)
         }
+    }
+
+    private fun addTextFieldBelow(
+        buttonAdd: MaterialButton,
+        textFieldAndCrossButton: View?,
+        placeToAdd: LinearLayout
+    ) {
+        placeToAdd.removeView(buttonAdd)
+        placeToAdd.addView(textFieldAndCrossButton)
+        placeToAdd.addView(buttonAdd)
     }
 
 
@@ -72,7 +104,8 @@ class MainActivity : AppCompatActivity() {
         val textInputLayout = TextInputLayout(this)
         textInputLayout.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            1.0f
         )
         //textInputLayout.hint = "Description fact"
         val textInputEditText = TextInputEditText(textInputLayout.context)
@@ -81,6 +114,7 @@ class MainActivity : AppCompatActivity() {
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
         textInputLayout.addView(textInputEditText)
+
         return textInputLayout
     }
 
